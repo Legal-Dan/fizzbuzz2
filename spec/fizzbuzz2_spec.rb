154b56441c343bdbd41f5737b54180d807012d96
Daniel Clements <clemies2000@hotmail.com>
2021-08-18 11:05:49 +0100

    Add final tests to make method work as expected

--- spec/fizzbuzz2_spec.rb
+++ spec/fizzbuzz2_spec.rb
@@ -10,4 +10,13 @@ describe 'fizzbuzz2' do
   it 'return number when 7 is passed' do
     expect(fizzbuzz2(7)).to eq 7
   end
+  it 'return fizz when 6 is passed' do
+    expect(fizzbuzz2(6)).to eq 'fizz'
+  end
+  it 'return buzz when 10 is passed' do
+    expect(fizzbuzz2(10)).to eq 'buzz'
+  end
+  it 'return fizzbuzz when 30 is passed' do
+    expect(fizzbuzz2(30)).to eq 'fizzbuzz'
+  end
 end
