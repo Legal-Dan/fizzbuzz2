b6e45d46664c0c0910b5a7665151ddd8b324f17e
LauraMaddaluno <lauralondon.4@gmail.com>
2021-08-18 11:53:28 +0000

    get rid of old method

--- lib/fizzbuzz2.rb
+++ lib/fizzbuzz2.rb
@@ -1,17 +1,8 @@
 def fizzbuzz2(number)
-#   if number % 3 == 0 && number % 5 ==0
-#     "fizzbuzz"
-#   elsif number % 3 == 0
-#     "fizz"
-#   elsif number % 5 == 0 
-#     "buzz"
-#   else 
-#     number
-#   end
-#end
-
+    
   return "fizzbuzz" if number % 3 == 0 && number % 5 ==0
-  return  "fizz" if number % 3 == 0
+  return "fizz" if number % 3 == 0
   return "buzz" if number % 5 == 0
-  number #if number % 3 != 0 && number % 5 !=0
+  number 
+  
 end
